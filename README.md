e985ce593675f08df89e56ce68c8522b5dcc7844
Lambert Timmermans <ltimmermans@fortinet.com>
2024-06-28 11:22:40 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+TEC Analytics Registration Page
+
+This page checks one in to a workshop, assigns a us_id, and stores the data provided.
---
+++ script.js
@@ -0,0 +1,41 @@
+function validateForm() {
+    const name = document.getElementById("name").value;
+    const company = document.getElementById("company").value;
+    const email = document.getElementById("email").value;
+    const nameError = document.getElementById("name-error");
+    const uid = crypto.randomUUID();
+    document.getElementById('uid').value = uid;
+    
+    const companyError = document.getElementById(
+        "company-error"
+    );
+    const emailError = document.getElementById(
+        "email-error"
+    );
+
+    nameError.textContent = "";
+    companyError.textContent = "";
+    emailError.textContent = "";
+
+    let isValid = true;
+
+    if (name === "" || /\d/.test(name)) {
+        nameError.textContent =
+            "Please enter your name properly.";
+        isValid = false;
+    }
+
+    if (company === "") {
+        companyError.textContent =
+            "Please enter your company.";
+        isValid = false;
+    }
+
+    if (email === "" || !email.includes("@")) {
+        emailError.textContent =
+            "Please enter a valid email address.";
+        isValid = false;
+    }
+
+    return isValid;
+}
